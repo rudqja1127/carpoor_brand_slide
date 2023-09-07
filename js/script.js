8066959a428e12761883d355337141c56b96bd3a
rudqja1127 <rudqja8297@naver.com>
2023-09-07 12:37:37 +0900

    브랜드선택

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
